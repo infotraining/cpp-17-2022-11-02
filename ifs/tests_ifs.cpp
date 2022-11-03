6139d2bf9209301a2a47efac2221bf93c206ec91
Krystian Piękoś <krystian.piekos@infotraining.pl>
2022-11-03 11:26:30 +0100

    constexpr if

--- ifs/tests_ifs.cpp
+++ ifs/tests_ifs.cpp
@@ -19,7 +19,7 @@ TEST_CASE("if with initializer")
     {
         std::cout << "Found: " << *pos << "\n";
     }
-    else if(auto pos2 = std::find(begin(vec), end(vec), 42); pos2 != end(vec) )
+    else if (auto pos2 = std::find(begin(vec), end(vec), 42); pos2 != end(vec))
     {
         std::cout << "Item 665 not found!\n";
         assert(pos == end(vec));
@@ -41,7 +41,7 @@ TEST_CASE("if with mutex")
 
     SECTION("thread#1")
     {
-        std::lock_guard lk {mtx_q_msg};
+        std::lock_guard lk{mtx_q_msg};
         q_msg.push("START");
     }
 
@@ -63,9 +63,9 @@ TEST_CASE("if with mutex")
 
 TEST_CASE("if with init + structured bindings")
 {
-    std::map<int, std::string> dict = { {1, "one"}, {2, "two"}};
+    std::map<int, std::string> dict = {{1, "one"}, {2, "two"}};
 
-    if (const auto&[pos, was_inserted] = dict.emplace(2, "dwa"); was_inserted)
+    if (const auto& [pos, was_inserted] = dict.emplace(2, "dwa"); was_inserted)
     {
         const auto& [key, value] = *pos;
         std::cout << key << " was inserted with value " << value << "\n";
@@ -75,4 +75,101 @@ TEST_CASE("if with init + structured bindings")
         const auto& [key, value] = *pos;
         std::cout << key << " was already in map with value " << value << "\n";
     }
+}
+
+////////////////////////////////////////////////////////
+// constexpr if
+
+namespace BeforeCpp17
+{
+    template <typename T>
+    auto is_power_of_2(T value) -> std::enable_if_t<std::is_integral<T>::value, bool>
+    {
+        return value > 0 && (value & (value - 1)) == 0;
+    }
+
+    template <typename T>
+    auto is_power_of_2(T value) -> std::enable_if_t<std::is_floating_point<T>::value, bool>
+    {
+        int exponent;
+        const T mantissa = std::frexp(value, &exponent);
+        return mantissa == static_cast<T>(0.5);
+    }
+}
+
+template <typename T>
+bool is_power_of_2(T value)
+{
+    if constexpr (std::is_integral_v<T>)
+    {
+        return value > 0 && (value & (value - 1)) == 0;
+    }
+    else
+    {
+        int exponent;
+        const T mantissa = std::frexp(value, &exponent);
+        return mantissa == static_cast<T>(0.5);
+    }
+}
+
+TEST_CASE("constexpr if")
+{
+    REQUIRE(is_power_of_2(4));
+    REQUIRE(is_power_of_2(8));
+    REQUIRE(is_power_of_2(32));
+    REQUIRE(is_power_of_2(77) == false);
+
+    REQUIRE(is_power_of_2(8.0));
+}
+
+namespace BeforeCpp17
+{
+    void print()
+    {
+        std::cout << "\n";
+    }
+
+    template <typename THead, typename... TTail>
+    void print(THead head, TTail... tail)
+    {
+        std::cout << head << " ";
+        print(tail...);
+    }
+}
+
+namespace SinceCpp17
+{
+    template <typename THead, typename... TTail>
+    void print(THead head, TTail... tail)
+    {
+        std::cout << head << " ";
+        
+        if constexpr(sizeof...(tail))
+        {
+            print(tail...);
+        }
+        else
+        {
+            std::cout << "\n";
+        }
+    }
+}
+
+TEST_CASE("constexpr if with variadic templates")
+{
+    BeforeCpp17::print(1, 2.13, "test"); // print(head = 1, tail = (2.13, "test")) // cout << 1
+                            //       -> print(head = 2.13, tail = ("test"))  // cout << 2.13
+                            //                -> print(head = "test", tail = ())  // cout << "test"
+                            //                         -> print()                     // cout << "\n"
+}
+
+template <typename... T1, typename... T2>
+void print_with_prefixes(std::tuple<T1...> prefixes, T2... args)
+{
+    BeforeCpp17::print(args...);
+}
+
+TEST_CASE("many tails")
+{
+    print_with_prefixes(std::tuple{"one", "two", "three"}, 1, 2, 3);
 }
