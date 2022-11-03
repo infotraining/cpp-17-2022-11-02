0be091ecd8034b0e2bc61e0172c13f01c235b19b
Krystian Piękoś <krystian.piekos@infotraining.pl>
2022-11-03 11:55:17 +0100

    constexpr_if - solution

--- _exercises/constexpr-if-ex/constexprif_ex.cpp
+++ _exercises/constexpr-if-ex/constexprif_ex.cpp
@@ -6,31 +6,48 @@
 
 using namespace std;
 
+// https://en.cppreference.com/w/cpp/iterator/iterator_traits
+
+
+
 template <typename Iterator>
-void advance_it(Iterator& it, size_t n)
+auto advance_it(Iterator& it, size_t n)
 {
+    if constexpr(is_base_of_v<random_access_iterator_tag, typename iterator_traits<Iterator>::iterator_category>)
+    {
+        it += n;
+        return std::random_access_iterator_tag{};
+    }
+    else
+    {
+        while(n--)
+        {
+            ++it;
+        }
+        return std::input_iterator_tag{};
+    }
 }
 
 TEST_CASE("constexpr-if with iterator categories")
 {
-    SECTION("random_access_iterator")
+    SECTION("random_access_iterator - it += n")
     {
         vector<int> data = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
         auto it = data.begin();
 
-        advance_it(it, 3);
+        std::random_access_iterator_tag result = advance_it(it, 3);
 
         REQUIRE(*it == 4);
     }
 
-    SECTION("input_iterator")
+    SECTION("input_iterator - n-times ++it")
     {
         list<int> data = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
         auto it = data.begin();
 
-        advance_it(it, 3);
+        std::input_iterator_tag result = advance_it(it, 3);
 
         REQUIRE(*it == 4);
     }
