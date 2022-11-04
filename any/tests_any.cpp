8789643afbeae2f4c2c87a67c04b96fce964f90f
Krystian Piękoś <krystian.piekos@infotraining.pl>
2022-11-04 16:15:43 +0100

    Variant

--- any/tests_any.cpp
+++ any/tests_any.cpp
@@ -5,9 +5,80 @@
 #include <numeric>
 #include <string>
 #include <vector>
+#include <map>
 
 using namespace std;
 
+struct Data
+{
+    int a, b;
+};
+
 TEST_CASE("any")
 {
+    std::any anything;
+
+    REQUIRE(anything.has_value() == false);
+
+    anything = 42;
+    anything = 3.14;
+    anything = "text"s;
+    anything = Data{10, 20};
+    anything = std::vector{1, 2, 3};
+
+    SECTION("access to any - getting a copy")
+    {
+        auto vec = std::any_cast<std::vector<int>>(anything);
+        REQUIRE(vec == std::vector{1, 2, 3});
+
+        REQUIRE_THROWS_AS(std::any_cast<std::string>(anything), std::bad_any_cast);
+    }
+
+    SECTION("access to any - getting a original value")
+    {
+        auto* vec = std::any_cast<std::vector<int>>(&anything);
+
+        if (vec)
+            REQUIRE(*vec == std::vector{1, 2, 3});
+
+        REQUIRE(std::any_cast<std::string>(&anything) == nullptr);
+    }
+
+    SECTION("type")
+    {
+        anything = Data{67, 55};
+
+        const std::type_info& type_desc = anything.type();
+        std::cout << "anything stores: " << type_desc.name() << "\n";
+    }
+}
+
+class DynamicMap
+{
+    std::map<std::string, std::any> dict_;
+public:
+    template <typename T>
+    bool insert(std::string key, T value) 
+    {
+        return dict_.emplace(std::move(key), std::move(value)).second;
+    }
+
+    template <typename T>
+    T get(const std::string& key) const
+    {
+
+        return std::any_cast<T>(dict_.at(key));
+    }
+};
+
+TEST_CASE("dynamic map")
+{
+    DynamicMap dm;
+
+    dm.insert("age", 42);
+    dm.insert("name", "Jan"s);
+
+    REQUIRE(dm.get<int>("age") == 42);
+    REQUIRE(dm.get<std::string>("name") == "Jan");
+    REQUIRE_THROWS_AS(dm.get<int>("name"), std::bad_any_cast);
 }
--- variant/tests_variants.cpp
+++ variant/tests_variants.cpp
@@ -9,6 +9,102 @@
 
 using namespace std;
 
+struct Data
+{
+    int a, b;
+};
+
 TEST_CASE("variant")
 {
+    std::variant<int, double, std::string, Data> var1;
+
+    REQUIRE(std::holds_alternative<int>(var1));
+    REQUIRE(std::get<int>(var1) == 0);
+
+    var1 = 42;
+    var1 = 3.14;
+    var1 = "text"s;
+    var1 = Data{42, 665};
+
+    auto& data = std::get<Data>(var1);
+    REQUIRE(data.a == 42);
+    REQUIRE(data.b == 665);
+
+    REQUIRE_THROWS_AS(std::get<std::string>(var1), std::bad_variant_access);
+
+    Data* ptr_data = std::get_if<Data>(&var1);
+    if (ptr_data)
+    {
+        REQUIRE(ptr_data->a == 42);
+    }
+
+    REQUIRE(var1.index() == 3);
+}
+
+struct Printer
+{
+    // std::string_view operator()(int x) const
+    // {
+    //     std::cout << "Value: " << x << "\n";
+    //     return typeid(x).name();
+    // }
+
+    // std::string_view operator()(double x) const
+    // {
+    //     std::cout << "Value: " << x << "\n";
+    //     return typeid(x).name();
+    // }
+
+    // std::string_view operator()(const std::string& x) const
+    // {
+    //     std::cout << "Value: " << x << "\n";
+    //     return typeid(x).name();
+    // }
+
+    template <typename T>
+    std::string_view operator()(const T& x) const
+    {
+        std::cout << "Value: " << x << "\n";
+        return typeid(x).name();
+    }
+
+    std::string_view operator()(const Data& x) const
+    {
+        std::cout << "Value: Data{" << x.a << ", " << x.b << "}\n";
+        return typeid(x).name();
+    }
+};
+
+template <typename... Ts>
+struct overload : Ts...
+{
+    using Ts::operator()...;
+};
+
+// deduction guide
+template <typename... Ts>
+overload(Ts...) -> overload<Ts...>;
+
+TEST_CASE("visiting variant")
+{
+    SECTION("with functor Printer")
+    {
+        std::variant<int, double, std::string, Data> var1;
+        var1 = Data{42, 665};
+        var1 = "text"s;
+
+        std::string_view type_name = std::visit(Printer{}, var1);
+        std::cout << "type: " << type_name << "\n";
+    }
+
+    SECTION("with overload")
+    {
+        std::variant<int, double, std::string, Data> var2 = "text"s;
+
+        auto printer = overload{
+            [](const auto& var) { std::cout << "Value: " << var << "\n"; },
+            [](const Data& d) { std::cout << "Value: Data{" << d.a << ", " << d.b << "}\n"; }};
+
+        std::visit(printer, var2);
+    }
 }
