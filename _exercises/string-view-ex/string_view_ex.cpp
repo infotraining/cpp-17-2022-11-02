39c7029c6c06e99eb8e1785305ad3850c56068e5
Krystian Piękoś <krystian.piekos@infotraining.pl>
2022-11-07 08:58:00 +0100

    Extra exercises

---
+++ _exercises/string-view-ex/string_view_ex.cpp
@@ -0,0 +1,31 @@
+#include <algorithm>
+#include <catch2/catch_test_macros.hpp>
+#include <iostream>
+#include <set>
+#include <string>
+#include <string_view>
+#include <vector>
+
+using namespace std;
+
+TEST_CASE("split with spaces")
+{
+    const char* text = "one two three four";
+
+    std::vector<std::string_view> words = split_text(text);
+
+    auto expected = {"one", "two", "three", "four"};
+
+    REQUIRE(equal(begin(expected), end(expected), begin(words)));
+}
+
+TEST_CASE("split with commas")
+{
+    string text = "one,two,three,four";
+
+    auto words = split_text(text);
+
+    auto expected = {"one", "two", "three", "four"};
+
+    REQUIRE(equal(begin(expected), end(expected), begin(words)));
+}
